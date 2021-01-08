55eba77d2d04a21b8f030929f5ef43631c98581e
Filimonov Kostya <konstantinfilimonou@gmail.com>
2021-01-09 01:05:21 +0300

    Tested a request to the server to get time and ping. Create testAPI

--- src/Components/Registration/Registration.tsx
+++ src/Components/Registration/Registration.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from "./Registration.module.css";
+import {getTimeAPI} from "../../Api/myTestApi";
 
 
 type RegistrationPropsType = {}
 
 const Registration: React.FC<RegistrationPropsType> = () => {
+    const [time, setTime] = useState(Date.now())
+    const [ping, setPing] = useState(0)
+
+    useEffect(() => {
+        getTimeAPI(time.toString())
+            .then(res => {
+                console.log(res)
+                setTime(time)
+                setPing(res.data.ping)
+            })
+    }, [time])
+
     return <div className={style.wrapper}>
         <h1>Registration</h1>
+        <h3>Test Time Request : {time}</h3>
+        <h3>Test Ping Request : {ping}</h3>
     </div>
 };
 
