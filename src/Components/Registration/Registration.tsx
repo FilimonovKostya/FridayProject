10ccfec54e9f9d6e9ed54db0e32e85f4059b75f3
Filimonov Kostya <konstantinfilimonou@gmail.com>
2021-01-09 19:35:30 +0300

    add Registration requests. All Ok. add TypeResponseError.

--- src/Components/Registration/Registration.tsx
+++ src/Components/Registration/Registration.tsx
@@ -4,32 +4,66 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'http://localhost:7542/2.0/',
-    withCredentials:true
+    withCredentials: true
 })
 
-const getTimeAPI = (time: string) => {
-    return instance.get(`ping?frontTime=${time}`)
+type RegistrationRequestType = {
+    email: string
+    password: string
+}
+
+type ErrorResponseType = {
+    email: string
+    error: string
+    in: string
+}
+
+const registrationAPI = () => {
+    return {
+        getTime: (time: string) => {
+            return instance.get(`ping?frontTime=${time}`)
+        },
+        registration: (dataReg: RegistrationRequestType) => {
+            return instance.post(`auth/register`, {...dataReg})
+        }
+    }
 }
 
 type RegistrationPropsType = {}
 
 const Registration: React.FC<RegistrationPropsType> = () => {
-    const [time, setTime] = useState(Date.now())
-    const [ping, setPing] = useState(0)
+    const [time, setTime] = useState<string>(Date.now().toString())
+    const [ping, setPing] = useState<number>(0)
+    const email = 'konstantinfilimonou@gmail.com'
+    const password = 'KOSTYA1234END.'
 
     useEffect(() => {
-        getTimeAPI(time.toString())
+        registrationAPI().getTime(time)
             .then(res => {
-                console.log(res)
                 setTime(time)
                 setPing(res.data.ping)
             })
     }, [time])
 
+    const onClickHandler = () => {
+        registrationAPI().registration({email, password})
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((error:ErrorResponseType) => {
+                console.log(error)
+            })
+    }
+
     return <div className={style.wrapper}>
         <h1>Registration</h1>
         <h3>Test Time Request : {time}</h3>
         <h3>Test Ping Request : {ping}</h3>
+        <form>
+            <div><input type="text" placeholder={'Enter email'} value={email} onChange={() => {}}/></div>
+            <div><input type="text" placeholder={'Enter password'} value={password} onChange={() => {}}/></div>
+            <button onClick={onClickHandler}> Submit</button>
+        </form>
     </div>
 };
 
