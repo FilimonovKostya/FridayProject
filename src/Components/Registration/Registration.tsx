2c72582aa7bc6fd8f0c83892c4a6b10031d06db4
Filimonov Kostya <konstantinfilimonou@gmail.com>
2021-01-09 21:37:26 +0300

    add Types:ErrorResponse,RegistrationsRequest/Response.Add conditions registration or already registr?

--- src/Components/Registration/Registration.tsx
+++ src/Components/Registration/Registration.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import style from "./Registration.module.css";
 import axios from "axios";
+import {Redirect} from "react-router-dom";
+import {path} from "../../App";
 
 const instance = axios.create({
     baseURL: 'http://localhost:7542/2.0/',
@@ -12,11 +14,31 @@ type RegistrationRequestType = {
     password: string
 }
 
-type ErrorResponseType = {
-    email: string
-    error: string
-    in: string
+type RegistrationResponseType = {
+    addedUser: {
+        created: string
+        email: string
+        isAdmin: boolean
+        name: string
+        publicCardPacksCount: number
+        rememberMe: boolean
+        updated: string
+        verified: boolean
+        __v: number
+        _id: string
+    }
 }
+
+type ErrorResponseType = {
+    response: {
+        data: {
+            email: string
+            error: string
+            in: string
+        }
+    }
+}
+
 
 const registrationAPI = () => {
     return {
@@ -24,7 +46,7 @@ const registrationAPI = () => {
             return instance.get(`ping?frontTime=${time}`)
         },
         registration: (dataReg: RegistrationRequestType) => {
-            return instance.post(`auth/register`, {...dataReg})
+            return instance.post<RegistrationResponseType>(`auth/register`, {...dataReg})
         }
     }
 }
@@ -34,6 +56,7 @@ type RegistrationPropsType = {}
 const Registration: React.FC<RegistrationPropsType> = () => {
     const [time, setTime] = useState<string>(Date.now().toString())
     const [ping, setPing] = useState<number>(0)
+    let [isRedirectProfile, setIsRedirectProfile] = useState<boolean>(false)
     const email = 'konstantinfilimonou@gmail.com'
     const password = 'KOSTYA1234END.'
 
@@ -48,20 +71,32 @@ const Registration: React.FC<RegistrationPropsType> = () => {
     const onClickHandler = () => {
         registrationAPI().registration({email, password})
             .then((res) => {
-                console.log(res)
+                const dataAboutUser = res.data.addedUser
+                if(Object.keys(dataAboutUser).length === 10){
+                    console.log('Успешно зареганы')
+                }
             })
-            .catch((error:ErrorResponseType) => {
-                console.log(error)
+            .catch((error: ErrorResponseType) => {
+                if (error.response.data.in === 'createUser') {
+                    console.log('Ok')
+                    setIsRedirectProfile(!isRedirectProfile)
+                }
             })
+
+    }
+
+    if (isRedirectProfile) {
+        return <Redirect to={path.PROFILE}/>
     }
 
     return <div className={style.wrapper}>
         <h1>Registration</h1>
-        <h3>Test Time Request : {time}</h3>
         <h3>Test Ping Request : {ping}</h3>
         <form>
-            <div><input type="text" placeholder={'Enter email'} value={email} onChange={() => {}}/></div>
-            <div><input type="text" placeholder={'Enter password'} value={password} onChange={() => {}}/></div>
+            <div><input type="text" placeholder={'Enter email'} value={email} onChange={() => {
+            }}/></div>
+            <div><input type="text" placeholder={'Enter password'} value={password} onChange={() => {
+            }}/></div>
             <button onClick={onClickHandler}> Submit</button>
         </form>
     </div>
